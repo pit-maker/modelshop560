04b67d28718d06787ecbd56b7de32012449a13a9
pit-maker <pgrishchenko1@gmail.com>
2026-06-28 17:03:16 +0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnSOhGm0J6oTXW8lFSd9y5lQGZ4KQrVs8SGnHd77_PQDbiZunmYStxQLWtQStBgC7Fl5d32nYuP_lqGa3f2k-5-8aaEhsicMjkVho0-zwqtSc4dhurN9QLToibv_RcgHAqYoTGU1vaHu73xWIk2HL_D52bzSLiVXnWE3aCwV6eH6g4zNUrS4OBw4-JDq1yRf1aR_SoVonMIvBAlnBfAYDfmQrmud_AKLpN6JtwLe9IhVQeOPwlC1JwpHmq6SOT0SZTS1RD2GZWUyORw1hzi0WPp645Fetg&lib=MlRQs5rfKg_r8UnphOtcUBKetveII9RHp";
+const API_URL = "https://script.google.com/macros/s/AKfycbx9sT--dQOpkzDRzPleJH71P-nlUCx0JQytLU3N5AKEp5fsUlxirzcagoMGH-Q80IcVTw/exec";
 
 export async function loadProducts() {
   try {
