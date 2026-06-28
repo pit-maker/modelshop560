975698fd31332791b95d3e8d68b68e9cc7a9cfd7
pit-maker <pgrishchenko1@gmail.com>
2026-06-28 16:39:35 +0300

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -9,22 +9,34 @@ export function renderProducts(container, products) {
 
     products.forEach(product => {
 
-        const card = document.createElement("div");
-        card.className = "card";
+        container.appendChild(
+            createProductCard(product)
+        );
 
-        const stock = product.stock === "В наличии"
-            ? '<span class="stock in">🟢 В наличии</span>'
-            : '<span class="stock out">🔴 Нет в наличии</span>';
-        
-        card.innerHTML = `
+    });
+
+}
+
+function createProductCard(product) {
+
+    const card = document.createElement("div");
+    card.className = "card";
+
+    const stockClass =
+        product.stock === "В наличии" ? "in" : "out";
+
+    const stockText =
+        product.stock === "В наличии"
+            ? "🟢 В наличии"
+            : "🔴 Нет в наличии";
+
+    card.innerHTML = `
         <img
             class="product-image"
             src="${product.image || "img/no-photo.png"}"
             alt="${product.name}"
-            loading="lazy"
-            onerror="this.src='img/no-photo.png'"
         >
-    
+
         <div class="card-body">
 
             <h3>${product.name}</h3>
@@ -32,28 +44,32 @@ export function renderProducts(container, products) {
             <div class="article">
                 Арт. ${product.article}
             </div>
-    
+
             <div class="manufacturer">
                 ${product.manufacturer}
             </div>
-    
+
             <div class="scale">
                 Масштаб: ${product.scale}
             </div>
 
-            <div class="scale">
-                Масштаб: ${stock}
+            <div class="stock ${stockClass}">
+                ${stockText}
             </div>
 
             <div class="price">
                 ${product.price} ${product.currency}
             </div>
-    
+
         </div>
     `;
 
-        container.appendChild(card);
+    const image = card.querySelector("img");
 
+    image.addEventListener("error", () => {
+        image.src = "img/no-photo.png";
     });
+
+    return card;
 
 }
