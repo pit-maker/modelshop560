addf2b170310eee2b730459b068961ce1997c925
pit-maker <pgrishchenko1@gmail.com>
2026-06-28 16:14:17 +0300

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -13,18 +13,36 @@ export function renderProducts(container, products) {
         card.className = "card";
 
         card.innerHTML = `
+        <img
+            class="product-image"
+            src="${product.image || "img/no-photo.png"}"
+            alt="${product.name}"
+            loading="lazy"
+            onerror="this.src='img/no-photo.png'"
+        >
+    
+        <div class="card-body">
+
             <h3>${product.name}</h3>
 
-            <p><strong>Артикул:</strong> ${product.article}</p>
+            <div class="article">
+                Арт. ${product.article}
+            </div>
+    
+            <div class="manufacturer">
+                ${product.manufacturer}
+            </div>
+    
+            <div class="scale">
+                Масштаб: ${product.scale}
+            </div>
 
-            <p>${product.manufacturer}</p>
-
-            <p>${product.scale}</p>
-
-            <p class="price">
+            <div class="price">
                 ${product.price} ${product.currency}
-            </p>
-        `;
+            </div>
+    
+        </div>
+    `;
 
         container.appendChild(card);
 
