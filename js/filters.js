c317e2ca85e0908ed6500411846af0ccf4f2e7f3
pit-maker <pgrishchenko1@gmail.com>
2026-06-28 15:40:04 +0300

    Update filters.js

--- js/filters.js
+++ js/filters.js
@@ -1,3 +1,46 @@
+import { state } from "./state.js";
+
 export function applyFilters() {
+
+    state.filteredProducts = state.products.filter(product => {
+
+        // Поиск
+        if (state.filters.search) {
+
+            const text = (
+                `${product.name} ${product.article}`
+            ).toLowerCase();
+
+            if (!text.includes(state.filters.search))
+                return false;
+        }
+
+        // Производитель
+        if (
+            state.filters.manufacturer &&
+            product.manufacturer !== state.filters.manufacturer
+        ) {
+            return false;
+        }
+
+        // Масштаб
+        if (
+            state.filters.scale &&
+            product.scale !== state.filters.scale
+        ) {
+            return false;
+        }
+
+        // Категория
+        if (
+            state.filters.category &&
+            product.category !== state.filters.category
+        ) {
+            return false;
+        }
+
+        return true;
+
+    });
 
 }
